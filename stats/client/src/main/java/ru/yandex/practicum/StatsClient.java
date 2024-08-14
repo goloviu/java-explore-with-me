48e6b2bc69c8612f50aab3edc56acbefb2585535
Dmitry <golovin.goloviu@yandex.ru>
2024-08-14 21:03:19 +0300

    fix: удалить неиспользуемый импорт

--- stats/client/src/main/java/ru/yandex/practicum/StatsClient.java
+++ stats/client/src/main/java/ru/yandex/practicum/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum;
 
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
